246ce68d622dbb5d08c735811fa24e02f2cbf23f
S197615 <43639812+S197615@users.noreply.github.com>
2018-10-03 18:21:58 +0100

    Create ex4.py

---
+++ ex4.py
@@ -0,0 +1,16 @@
+my_name = "Jordan Allen"
+my_age = 19
+my_height = 73
+my_weight = 160
+my_eyes = "Grey"
+my_hair = "Black"
+is_heavy = my_weight > 3000
+
+print(f"let's talk about {my_name}.")
+print(f"He is {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"It is {is_heavy} that he is overweight.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}")

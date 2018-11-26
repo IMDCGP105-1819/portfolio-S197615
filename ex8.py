744f79a1a8485f8e48efc01c601307dd31bbc161
S197615 <43639812+S197615@users.noreply.github.com>
2018-11-26 18:56:18 +0000

    Add files via upload

--- ex8.py
+++ ex8.py
@@ -4,18 +4,22 @@ current_savings = 0
 r = 1.04
 annual_salary = input("Enter your annual salary: ")
 portion_saved = input("Enter the percent of your salary to save, as a decimal: ")
-monthly_salary = int(annual_salary)/12
-monthly_input = float(monthly_salary) * float(portion_saved)
 months = 0
 semi_annual_raise = input("Enter a semi annual raise as a decimal: ")
+semi_annual_raise = float(semi_annual_raise) + 1
+counter = 0
 
 while current_savings < portion_deposit:
+    pay_rise = float(annual_salary) * float(semi_annual_raise)
+    monthly_salary = int(annual_salary) / 12
+    monthly_input = float(monthly_salary) * float(portion_saved)
     current_savings = current_savings * r
     current_savings = current_savings + monthly_input
     months = months + 1
-    pay_rise = months /6
-    if pay_rise == type(int) > 0:
-        annual_salary = annual_salary * semi_annual_raise
+    counter = counter + 1
+    if counter / 6 == 1:
+        annual_salary = pay_rise
+        counter = 0
+
 else:
     print("Number of months: " + str(months))
-
